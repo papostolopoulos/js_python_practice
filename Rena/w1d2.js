8db387d7ad67575e2e93d95545b731a84f4753af
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-12-30 20:45:59 -0800

    Created w1d2.js files

---
+++ Rena/w1d2.js
@@ -0,0 +1,43 @@
+//------------------------------------//
+//            Week 1 Day 2            //
+//------------------------------------//
+
+/* logBetween
+
+Define a function logBetween(lowNum, highNum) that will print every number from lowNum to highNum, inclusive.
+Inclusive means that the the range that will be printed will include the lowNum and highNum.
+
+logBetween(-1, 2); // from -1 to 2
+logBetween(14, 6); //nothing printed
+logBetween(4,6); //from 4 to 6 */
+
+
+
+/* fizzBuzz
+
+3 and 5 are magic numbers.
+Define a function fizzBuzz(max) that takes a number and prints to the console every number
+from 0 to max that is divisible by either 3 or 5, but not both.
+
+TEST: fizzBuzz(20) should print numbers 3, 5, 6, 9, 10, 12, and 18 */
+
+
+
+/* isFactorOf
+
+Define a function isFactorOf(number, factor) that returns true if factor is a factor of number. Otherwise, false.
+
+isFactorOf(6,2); //true
+isFactorOf(-6, 2); //true
+isFactorOf(5,0); //false
+ */
+
+
+
+/* isPrime
+Define a function isPrime(number) that returns true if number is prime.
+Otherwise, false. Assume number is a positive integer.*/
+// isPrime(2); //true
+// isPrime(10); //false
+// isPrime(15485863); //true
+// isPrime(3548563); //false

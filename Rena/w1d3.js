112078dc9165f7e256d46454ef70b8fa4dc59a95
Mohammed Almahbashi <mohawow@gmail.com>
2018-01-02 07:21:05 -0800

    week1 day2 and some peresonal notes for mysql

---
+++ Rena/w1d3.js
@@ -0,0 +1,70 @@
+//------------------------------------//
+//            Week 1 Day 3            //
+//------------------------------------//
+
+/* Arrays
+logEach
+Write a function logEach(array) that prints every element of the array and its index to the console . Example:
+> logEach(["Anthony", "John", "Carson"]);
+0: Anthony
+1: John
+2: Carson */
+
+
+/* maxValue
+Write a function maxValue(array) that returns the largest value in the array. Assume array is an array of numbers.
+> maxValue([43, 12, 6, 2])
+43
+> maxValue([])
+null
+> maxValue([-4, -10, 0.43])
+0.43 */
+
+
+/* printRange
+
+Write a function printRange(start, end) that prints all the numbers from start to end.
+If a range doesn't exist (start > end), then print "Bad Range" instead. Example:
+
+> printRange(22, 24)
+22
+23
+24
+> printRange(5, 1)
+Bad Range */
+
+
+/* Functions
+isPrime
+Write a function isPrime(number) that returns a boolean indicating if number is prime or not.
+Assume number is a positive integer.
+
+> isPrime(2)
+true
+>isPrime(1693)
+true
+> isPrime(15)
+false
+> isPrime(303212)
+false */
+
+
+/* firstNPrimes
+Using isPrime, write a function firstNPrimes(n) that returns an array of the first n prime numbers.
+> firstNPrimes(0)
+[]
+> firstNPrimes(1)
+[2]
+> firstNPrimes(4)
+[2, 3, 5, 7] */
+
+
+/* sumOfNPrimes
+
+Using firstNPrimes, write a function sumOfNPrimes(n) that returns the sum of the first n prime numbers.
+> sumOfNPrimes(0)
+0
+> sumOfNPrimes(1)
+2
+> sumOfNPrimes(4)
+17 */

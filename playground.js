df1e1010438463d59fb282f84872af1ceae08dcd
Paraskevas Apostolopoulos <papost@fb.com>
2018-01-11 14:12:06 -0800

    winLottery() - 2

--- playground.js
+++ playground.js
@@ -7,7 +7,7 @@ function winLottery(){
   var userAnswer = prompt("Did you win the lottery ticket?");
   while (userAnswer.toLowerCase() !== "yes") {
     alert("Sorry, you have to keep on studying code or win the lottery");
-    winLottery();
+    userAnswer = prompt("Did you win the lottery ticket?");
   }
 
   alert("Congratulations. You won the lottery so you can live a life in prosperity. Remember your good friend Paris and give him something.");

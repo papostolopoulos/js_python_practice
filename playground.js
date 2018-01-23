70767a0b8877a24797323c5f4441f73ad10f4d82
Paraskevas Apostolopoulos <papost@fb.com>
2018-01-23 12:37:56 -0800

    feedback for w1d3 for Rena and Mohammed

--- playground.js
+++ playground.js
@@ -1,3 +1,74 @@
+function isPrime(number){
+ for(var i = 2; i < number; i++){
+ 	if(number % i === 0){
+ 		return false;
+ 	}
+ }
+ return true;
+}
+isPrime(303212);
+isPrime(1693);
+
+
+/* firstNPrimes
+0. Using isPrime,
+1. write a function firstNPrimes(n) OK
+2. that returns an array OK
+3. of the first n prime numbers.
+> firstNPrimes(0)
+[]
+> firstNPrimes(1)
+[2]
+> firstNPrimes(4)
+[2, 3, 5, 7] */
+
+
+
+
+function firstNPrimes(n){
+  var array = [];
+  for (var i = 2; i < 10; i++) {
+    if (isPrime(i) === true) {
+      array.push(i);
+    }
+  }
+
+  return array;
+}
+
+
+
+
+firstNPrimes(6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function mohammedsChallenge(money, haveTime){
   return money <= 10000 && haveTime >= 40 ? "Make some money first" : money <= 10000 && haveTime < 40 ? "Keep on working! More money needed" : money > 10000 && haveTime >= 40 ? "You have enough money. Time to code!" : "Keep writing code! You are doing it"
 }

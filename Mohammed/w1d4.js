d622a90db4cb2f7168377bfcf9acab1144798cc2
Paraskevas Apostolopoulos <papost@fb.com>
2018-01-26 08:48:00 -0800

    updated w1d4 exercise

--- Mohammed/w1d4.js
+++ Mohammed/w1d4.js
@@ -4,23 +4,39 @@
 
 /* Cookie Monster Project
 
-For this project, we are going to make a cookieMonster object.
-Type mocha in the command prompt/terminal to see all the failing tests. Your job will be to make them all pass.
-
-Look at the first failing test.
-It is expecting cookieMonster to have a property called name that evaluates to Fred.
-In cookieMonster.js, add the property name and set it to Fred.
-
-Run mocha again. Yay! The spec has passed.
-Now fix all the "initial properties" specs.
-
-We are going to create two methods #eat(food) and #isAlrightMeal(food):
-
-#eat(food): returns a score (integer). food is an array of of string food items.
-Every food item that is a favorite food add +2 to the score;
-good foods add +1;
-bad foods add -1;
-hated foods add -2.
-Make sure the spec passes.
-
-#isAlrightMeal(food): return true if the food score is within -1, 1. */
+Create an object that is called cookieMonster.
+The object should have:
+- A property called "name" with a value that equals to Fred.
+- A property that is called "foods" and values to a new object.
+This object should have the following key value pairs:
+  - awesomeFoods with an array that has values "cookies" and "milk"
+  - goodFoods with an array that has values "pizza", "banana", "apple"
+  - badFoods with an array that has values "salad", "cheese"
+  - hatedFoods with an array that has values "spinach", "brocolli"
+- A method called "eat" that takes has one parameter (food).
+This method needs to return a final score that is calculated based on the types of
+foods entered.
+There can be one or more arguments entered in the method each time it is run.
+  - The method needs to run through all the arguments and see if they are included
+  in the list of foods in the different categories.
+  - If the argument's value is included in any of the above categories, then it
+  should provide the following score:
+  +2 for an awesome food
+  +1 for a good food
+  -1 for a bad food
+  -1 for a hated food
+- A method called isAlrightMeal that does the following:
+  It needs to iterate through all the foods entered as arguments and print
+  the following strings for each one of them.
+  - For an awesome food Fred should respond: "(name) likes (food). Chocolate chip
+  cookie important to me... OM NOM NOM NOM NOM"
+  - For an alright food Fred should respond: "(food)? Me love poetry, and cookies!"
+  - For a bad food Fred should respond: "(food) or cookie?...(food)...cookie
+  Me Cookie Monster! This is no-brainer!"
+  - For hated foods Fred should respond: "(food)? Me want cookie"
+  These strings should be displayed in the console with a delay of one second each
+  At the end, there should be a string returned which should read as the following:
+  - If the score from the eat method is larger than two, then Fred should say:
+  "Me liked food. Me eat a cookie for you!"
+  - if the score is lower than three then Fred should say:
+  - "(name) did not like food. (name) wants to eat cookie!"
--- playground.js
+++ playground.js
@@ -38,6 +38,10 @@ function maxValue(array){
 
 }
 
+function sumOfNPrimes(n) {
+  
+}
+
 
 
 

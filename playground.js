c4530652b8e6a7379c888b5d34e2b4198de5018d
Paraskevas Apostolopoulos <papost@fb.com>
2018-01-30 17:18:54 -0800

    worked on cookieMonster

--- playground.js
+++ playground.js
@@ -1,81 +1,73 @@
-// Write a function maxValue(array)
-//that returns the largest value in the array.
-// Assume array is an array of numbers.
-// > maxValue([43, 12, 6, 2])
-// 43
-// > maxValue([])
-// null
-// > maxValue([-4, -10, 0.43])
-// 0.43 */
 
-
-function maxValue(array){
-  var largestValue = -Infinity;
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] > largestValue) {
-      largestValue = array[i]
-    }
+var cookieMonster = {
+  name: "Fred",
+  foods: {
+    awesomeFoods: ["cookies", "milk"],
+    goodFoods: ["pizza", "banana", "apple"],
+    badFoods: ["salad", "cheese"],
+    hatedFoods: ["spinach", "brocolli"]
+  },
+  eat: function(food){
+    console.log("Hello, my name is " + this.name + " and I like " + food);
   }
+};
 
-  return largestValue;
-}
+// - A method called "eat" that takes one parameter (food).
 
-
-function maxValue(array){
-  return array.sort((a, b) => {
-    a - b;
-  })[array.length - 1];
-
-}
+/*
+Study the following:
+1) arguments in JavaScript: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+2) The "this" keyword
+*/
 
 
+// cookieMonster.foods.goodFoods;
+// ["pizza", "banana", "apple"]
+// cookieMonster["foods"]["goodFoods"];
+// ["pizza", "banana", "apple"]
+// cookieMonster.foods["goodFoods"];
+// ["pizza", "banana", "apple"]
+// cookieMonster["foods"].goodFoods;
+// ["pizza", "banana", "apple"]
+//------------------------------------//
+//            Week 1 Day 4            //
+//------------------------------------//
 
-function maxValue(array){
-  var finalArr = array.sort(function(a, b){
-    return a - b;
-  });
-  return finalArr[finalArr.length - 1];
+/* Cookie Monster Project
 
-}
-
-function sumOfNPrimes(n) {
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function mohammedsChallenge(money, haveTime){
-  return money <= 10000 && haveTime >= 40 ? "Make some money first" : money <= 10000 && haveTime < 40 ? "Keep on working! More money needed" : money > 10000 && haveTime >= 40 ? "You have enough money. Time to code!" : "Keep writing code! You are doing it"
-}
-
-
-function winLottery(){
-  var userAnswer = prompt("Did you win the lottery ticket?");
-  while (userAnswer.toLowerCase() !== "yes") {
-    alert("Sorry, you have to keep on studying code or win the lottery");
-    userAnswer = prompt("Did you win the lottery ticket?");
-  }
-
-  alert(`Congratulations. You won the lottery so you can live a life in prosperity.
-  Remember your good friend Paris and give him something.`);
-}
-
-winLottery();
+Create an object that is called cookieMonster.
+The object should have:
+- A property called "name" with a value that equals to Fred.
+- A property that is called "foods" and values to a new object.
+This object should have the following key value pairs:
+  - awesomeFoods with an array that has values "cookies" and "milk"
+  - goodFoods with an array that has values "pizza", "banana", "apple"
+  - badFoods with an array that has values "salad", "cheese"
+  - hatedFoods with an array that has values "spinach", "brocolli"
+- A method called "eat" that takes one parameter (food).
+This method needs to return a final score that is calculated based on the types of
+foods entered as arguments.
+There can be one or more arguments entered in the method each time it is run.
+  - The method needs to run through all the arguments and see if they are included
+  in the list of foods in the different categories.
+  - If the argument's value is included in any of the above categories, then it
+  should provide the following score:
+  +2 for an awesome food
+  +1 for a good food
+  -1 for a bad food
+  -2 for a hated food
+- A method called isAlrightMeal that does the following:
+  It needs to iterate through all the foods entered as arguments and print
+  the following strings for each one of them.
+  - For an awesome food Fred should respond: "(name) likes (food). Chocolate chip
+  cookie important to me... OM NOM NOM NOM NOM"
+  - For an alright food Fred should respond: "(food)? Me love poetry, and cookies!"
+  - For a bad food Fred should respond: "(food) or cookie?...(food)...cookie
+  Me Cookie Monster! This is no-brainer!"
+  - For hated foods Fred should respond: "(food)? Me want cookie"
+  These strings should be displayed in the console with a delay of one second each
+  At the end, there should be a string returned which should read as the following:
+  - If the score from the eat method is larger than two, then Fred should say:
+  "Me liked food. Me eat a cookie for you!"
+  - if the score is lower than three then Fred should say:
+  - "(name) did not like food. (name) wants to eat cookie!"

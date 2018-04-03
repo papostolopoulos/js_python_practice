2e8f35b9ad7c52ff847a1a95b66087b3d8b9695a
Paraskevas Apostolopoulos <papost@fb.com>
2018-04-03 07:48:18 -0700

    recursion example in playground

--- playground.js
+++ playground.js
@@ -1,3 +1,23 @@
+//loops vs recursion 
+function addTwoFewTimes(num, counter) {
+  while (counter > 0) {
+    num += 2
+    counter -= 1
+  }
+  return num;
+}
+
+
+function addTwoFewTimes(num, counter){
+  num += 2
+  counter -= 1
+  if (counter === 0) {
+    return num;
+  }
+  addTwoFewTimes(num, counter);
+}
+
+
 
 // myIndexOf
 

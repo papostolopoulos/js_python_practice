7aa459011b83d0471a1f081a02e1dbf8e8968a1b
Rena Tan <rena.tanrx@gmail.com>
2018-04-08 19:43:17 -0700

    wk3,day2 updates

--- Rena/w3d2.js
+++ Rena/w3d2.js
@@ -3,33 +3,47 @@
 //------------------------------------//
 
 /* Clock
-For this mini-project, you will create a clock that ticks every second, displaying the current time.
+For this mini-project, you will create a clock that ticks every second, displaying 
+the current time.
 READ ALL OF THE INSTRUCTIONS FIRST! Ask questions if you don't understand something.
 
 Create a clock object;
 Give it a property totalSeconds that starts at 0; */
 
 var clock = {
-  //enter your code here
+ totalSeconds: 0;
 }
 
 /* Write a method getSeconds that will return the second count of our clock.
 It should return a value from 00 to 59, just like a normal clock.
-hint: Think about this is terms of totalSeconds. If totalSeconds equals 10, then this method should return "10".
+hint: Think about this is terms of totalSeconds. If totalSeconds equals 10, then this 
+method should return "10".
 If totalSeconds is 61 (01:01), this method should return "01".
-hint: Play with the modulus operator. If I modulo x by i (x % i), will the result ever be greater than i?
+hint: Play with the modulus operator. If I modulo x by i (x % i), will the result ever 
+be greater than i?
 hint: If the value is less than 9, you may have to append a 0 to the beginning
 
 Test you getSeconds method.
-Open node and manually set the totalSeconds and make sure getSeconds is giving you the right value. Test cases below:
+Open node and manually set the totalSeconds and make sure getSeconds is giving you the 
+right value. Test cases below:
 If clock.totalSeconds = 0, clock.getSeconds() will evaluate to "00"
 If clock.totalSeconds = 9, clock.getSeconds() will evaluate to "09"
 If clock.totalSeconds = 13, clock.getSeconds() will evaluate to "13"
 If clock.totalSeconds = 60, clock.getSeconds() will evaluate to "00"
 If clock.totalSeconds = 1342, clock.getSeconds() will evaluate to "22" */
 
-var clock = {
-  //copy your code from above and continue with new property
+
+
+function getSeconds(){
+  var clock = {
+    totalSeconds: 0;
+  }
+
+  if(totalSeconds < 60){
+    //trying to use .getSeconds() method - not sure if appropriate?
+    var totalTime = clock.getSeconds();
+  }
+  console.log(totalTime);
 }
 
 
@@ -38,11 +52,14 @@ var clock = {
 It should return a value from 00 to 59, just like a normal clock.
 
 hint: Write this in terms of totalSeconds, not the result of getSeconds?
-Can you determine why? Think about it and if you're having trouble, call over the instructor.
-hint: This will involve a few mathematical operations. How many minutes is 120 seconds? 2 minutes.
+Can you determine why? Think about it and if you're having trouble, call over 
+the instructor.
+hint: This will involve a few mathematical operations. How many minutes is 120 
+seconds? 2 minutes.
 What math do we intuitively use to determine that?
 hint: How many minutes are in 135 seconds? Still only 2 minutes.
-The remainder exists as the second count. You may have to use some Math (hint hint) for cases like this.
+The remainder exists as the second count. You may have to use some Math (hint hint) 
+for cases like this.
 
 Test your getMinutes method:
 
@@ -94,12 +111,14 @@ If startSecond was passed in (we'll make it optional), set totalSeconds to be eq
 Increment totalSeconds.
 Print the current time to the console.
 Using either setTimeout or setInterval, make this behavior repeat itself every second.
-Remember, arguments in Javascript are optional, so no need to worry about constantly setting the startSecond parameter.
+Remember, arguments in Javascript are optional, so no need to worry about constantly 
+setting the startSecond parameter.
 
 
 
 Test is out. When you're done, you'll have a clock that prints out a new time every second!
-Bonus: Try writing tick using both setTimeout and setInterval! I called my methods intervalTick and timeoutTick. */
+Bonus: Try writing tick using both setTimeout and setInterval! I called my methods
+intervalTick and timeoutTick. */
 
 var clock = {
   //copy your code from above and continue with new property

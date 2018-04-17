db5ad869324e82f5f95bd84a056a6633352a8fc4
Rena Tan <rena.tanrx@gmail.com>
2018-04-16 21:46:24 -0700

    wk3,day3

--- Rena/w3d3.js
+++ Rena/w3d3.js
@@ -5,13 +5,19 @@
 /*
 Higher-Order Functions
 Write a function myForEach(arr, cb) that accepts an array and a callback.
-It should pass each element, its corresponding index, and the array itself to the callback.
-Do not use the built-in Array.prototype.forEach method. The return value is irrelevant.
+It should pass each element, its corresponding index, and the array itself 
+to the callback.
+Do not use the built-in Array.prototype.forEach method. The return value 
+is irrelevant.
 
  1 is at position 0 in array [1,2,3]
  2 is at position 1 in array [1,2,3]
  3 is at position 2 in array [1,2,3] */
 
+
+ function myForEach(arr, cb){
+  
+ }
 
 
 /* Write a function mySelect(arr, cb) that accepts an array and a callback.
